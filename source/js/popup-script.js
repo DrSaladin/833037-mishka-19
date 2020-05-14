b967254a3925ace2cf606eb66785bf373fd4f74b
DrVitalii <dr.saladinn@yandex.ru>
2020-05-14 15:40:34 +0700

    14.05 prefinal

--- source/js/popup-script.js
+++ source/js/popup-script.js
@@ -6,8 +6,8 @@ var links = document.querySelectorAll(".catalog-item__order-cart");
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popupBlure.classList.contains("popup-cart-show")) {
-      popupBlure.classList.remove("popup-cart-show");
+    if (popupBlure.classList.contains("blure-background-popup-show")) {
+      popupBlure.classList.remove("blure-background-popup-show");
       popupBlure.classList.add("visually-hidden");
     }
   }
@@ -18,7 +18,7 @@ for (var i = 0; i < links.length; i++) {
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popupBlure.classList.remove("visually-hidden");
-    popupBlure.classList.add("popup-cart-show");
+    popupBlure.classList.add("blure-background-popup-show");
   });
 }
 
